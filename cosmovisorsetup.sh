c384d8599c0c344b41d9762183eed1c71318b908
Golden Ratio Staking <95374052+Golden-Ratio-Staking@users.noreply.github.com>
2022-01-11 21:13:26 +0100

    Update cosmovisorsetup.sh

--- cosmovisorsetup.sh
+++ cosmovisorsetup.sh
@@ -2,11 +2,6 @@
 # Get Cosmovisor
 go get github.com/cosmos/cosmos-sdk/cosmovisor/cmd/cosmovisor
 
-# Export Variables
-export DAEMON_NAME=$BINARY_NAME
-export DAEMON_HOME=$HOME/$BINARY_FOLDER
-source ~/.profile
-
 # Setup folders
 mkdir -p $DAEMON_HOME/cosmovisor/genesis/bin
 mkdir -p $DAEMON_HOME/cosmovisor/upgrades
@@ -17,24 +12,3 @@ cp /home/$USER/go/bin/$BINARY_NAME $DAEMON_HOME/cosmovisor/genesis/bin
   
 # Setup Cosmovisor Service
 sudo nano /etc/systemd/system/cosmovisor.service
-
-# Configure variables
-[Unit]
-Description=cosmovisor
-After=network-online.target
-
-[Service]
-User=$USER
-ExecStart=/home/$USER/go/bin/cosmovisor start
-Restart=always
-RestartSec=3
-LimitNOFILE=4096
-Environment="DAEMON_NAME=$BINARY_NAME"
-Environment="DAEMON_HOME=/home/$USER/$BINARY_FOLDER"
-Environment="DAEMON_ALLOW_DOWNLOAD_BINARIES=false"
-Environment="DAEMON_RESTART_AFTER_UPGRADE=true"
-Environment="DAEMON_LOG_BUFFER_SIZE=512"
-Environment="UNSAFE_SKIP_BACKUP=true"
-
-[Install]
-WantedBy=multi-user.target
