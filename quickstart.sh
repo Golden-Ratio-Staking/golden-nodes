ff64dca5139ffde295c103dc3ea756a627b6ba34
Golden Ratio Staking <95374052+Golden-Ratio-Staking@users.noreply.github.com>
2022-01-10 17:13:47 -0800

    Create quickstart.sh

---
+++ quickstart.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt install build-essential -y
+sudo apt-get install tasksel -y
+sudo apt-get install lightdm -y
+sudo tasksel
+reboot
