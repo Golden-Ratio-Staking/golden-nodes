e393c59524e9aa7acb8d414749737ae18be7c5b7
Golden Ratio Staking <95374052+Golden-Ratio-Staking@users.noreply.github.com>
2022-01-17 13:03:31 -0800

    Create gopath.sh

---
+++ gopath.sh
@@ -0,0 +1,6 @@
+!#bin/bash
+echo "export GOROOT=/usr/local/go"
+echo "export GOPATH=$HOME/go"
+echo "export GO111MODULE=on"
+echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin"
+echo "source ~/.profile"
