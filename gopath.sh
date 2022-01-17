5d6acf5cf9f00a84a76bdbedb6178f4cf22975fd
Golden Ratio Staking <95374052+Golden-Ratio-Staking@users.noreply.github.com>
2022-01-17 13:28:13 -0800

    Update gopath.sh

--- gopath.sh
+++ gopath.sh
@@ -1,4 +1,6 @@
 #!bin/bash
+wget https://golang.org/dl/go1.17.5.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.17.5.linux-amd64.tar.gz
 echo "export GOROOT=/usr/local/go"
 echo "export GOPATH=$HOME/go"
 echo "export GO111MODULE=on"
