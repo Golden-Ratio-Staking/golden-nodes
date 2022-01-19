f04b7572ce1a75871d0d61ae17459a7baeba8ad6
Golden Ratio Staking <95374052+Golden-Ratio-Staking@users.noreply.github.com>
2022-01-18 21:09:22 -0800

    Update gui.sh

--- gui.sh
+++ gui.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 sudo apt-get update && sudo apt-get upgrade -y
 sudo apt install build-essential -y
+sudo apt-get install lightdm -y
 sudo apt-get install tasksel -y
-sudo tasksel
+sudo apt-get install ubuntu-desktop
 reboot
