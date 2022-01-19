97d86f29b8fb44ed1591ce2fe26eff07167160f3
Golden Ratio Staking <95374052+Golden-Ratio-Staking@users.noreply.github.com>
2022-01-19 09:52:59 -0800

    Create bareserver.sh

---
+++ bareserver.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# update the local package list and install any available upgrades
+sudo apt-get update && sudo apt upgrade -y
+
+# Install Remote Desktop
+sudo apt install xrdp -y
+sudo systemctl enable xrdp
+
+# Install Nautilus
+sudo apt-get update
+sudo apt install seahorse-nautilus -y
+sudo nautilus -q
+
+# update the local package list and install any available upgrades
+sudo apt-get update && sudo apt upgrade -y
+
+# Install toolchain and ensure accurate time synchronization
+sudo apt install curl tar wget pkg-config libssl-dev jq build-essential git make ncdu -y
+
+# Install Golang (Go)
+wget https://golang.org/dl/go1.17.5.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.17.5.linux-amd64.tar.gz
+echo "export GOROOT=/usr/local/go"
+echo "export GOPATH=$HOME/go"
+echo "export GO111MODULE=on"
+echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin"
+echo "source ~/.profile"
