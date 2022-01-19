634d6fab1e6735fd68ab44d1973cb6492f7316e9
Golden Ratio Staking <95374052+Golden-Ratio-Staking@users.noreply.github.com>
2022-01-19 00:49:43 -0800

    Update gui.sh

--- gui.sh
+++ gui.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
-sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get update -y && sudo apt-get upgrade -y
 sudo apt upgrade -y
 sudo apt install xrdp -y
 sudo systemctl enable xrdp
-sudo apt install build-essential -y
 sudo apt-get install tasksel -y
 sudo apt-get install lightdm -y
 sudo tasksel
-sudo nano /etc/default/grub
-sudo update-grub
-sudo apt-get update -y && apt-get upgrade -y
 reboot
