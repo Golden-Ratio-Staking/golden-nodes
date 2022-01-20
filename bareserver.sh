6b4d6797c5bf7c39250455432293ed30180b21e7
Golden Ratio Staking <95374052+Golden-Ratio-Staking@users.noreply.github.com>
2022-01-19 16:34:41 -0800

    Update bareserver.sh

--- bareserver.sh
+++ bareserver.sh
@@ -21,8 +21,3 @@ sudo apt install curl tar wget pkg-config libssl-dev jq build-essential git make
 # Install Golang (Go)
 wget https://golang.org/dl/go1.17.5.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.17.5.linux-amd64.tar.gz
-echo "export GOROOT=/usr/local/go"
-echo "export GOPATH=$HOME/go"
-echo "export GO111MODULE=on"
-echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin"
-echo "source ~/.profile"
