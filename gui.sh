ffc73810fc5eeddedabc3415a70b6e2755650b26
Golden Ratio Staking <95374052+Golden-Ratio-Staking@users.noreply.github.com>
2022-01-21 16:38:22 -0800

    Update gui.sh

--- gui.sh
+++ gui.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 sudo apt-get update -y && sudo apt-get upgrade -y
-sudo apt upgrade -y
-sudo apt install xfce4 xfce4-goodies xorg dbus-x11 x11-xserver-utils -y
-sudo apt install xrdp -y
-sudo adduser xrdp ssl-cert
-sudo systemctl enable xrdp
-sudo systemctl restart xrdp
 sudo apt-get install tasksel -y
 sudo apt-get install lightdm -y
 sudo tasksel
