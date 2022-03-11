6db76d3cd987cd7b41818f3a041e8b4f4b819dad
Golden Ratio Staking <95374052+Golden-Ratio-Staking@users.noreply.github.com>
2022-03-10 18:06:38 -0800

    Update cosmovisorsetup.sh

--- cosmovisorsetup.sh
+++ cosmovisorsetup.sh
@@ -6,7 +6,7 @@ go get github.com/cosmos/cosmos-sdk/cosmovisor/cmd/cosmovisor
 mkdir -p $DAEMON_HOME/cosmovisor/genesis/bin
 mkdir -p $DAEMON_HOME/cosmovisor/upgrades
 
-# Place Genisis Binary in Folder
+# Place Binary in Folder
 cp /home/$USER/go/bin/$DAEMON_NAME $DAEMON_HOME/cosmovisor/genesis/bin
 
 # Setup Cosmovisor Service
