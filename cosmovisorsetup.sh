26eed4f58a6c978a7911bbe5afd7d194f5cc2e47
Golden Ratio Staking <95374052+Golden-Ratio-Staking@users.noreply.github.com>
2022-04-27 16:31:28 -0700

    Update to upgrade v1.0.0 -> v1.1.0 transition

--- cosmovisorsetup.sh
+++ cosmovisorsetup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-# Get Cosmovisor
-go get github.com/cosmos/cosmos-sdk/cosmovisor/cmd/cosmovisor
+# Get Cosmovisor, then upgrade it immediately to latest verison...you wont even notice
+go install github.com/cosmos/cosmos-sdk/cosmovisor/cmd/cosmovisor@v1.0.0
+go install github.com/cosmos/cosmos-sdk/cosmovisor/cmd/cosmovisor@v1.1.0
 
 # Setup folders
 mkdir -p $DAEMON_HOME/cosmovisor/genesis/bin
