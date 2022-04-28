0404019adea7367f86b2a8e1ba36b06a0f53f3c7
Golden Ratio Staking <95374052+Golden-Ratio-Staking@users.noreply.github.com>
2022-04-28 10:57:00 -0700

    Learned things. Deprecated.

--- gopath.sh
+++
@@ -1,8 +0,0 @@
-#!bin/bash
-wget https://golang.org/dl/go1.17.5.linux-amd64.tar.gz
-sudo tar -C /usr/local -xzf go1.17.5.linux-amd64.tar.gz
-echo "export GOROOT=/usr/local/go"
-echo "export GOPATH=$HOME/go"
-echo "export GO111MODULE=on"
-echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin"
-echo "source ~/.profile"
