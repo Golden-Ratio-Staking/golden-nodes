62aa5e5a3d84776d6668b3ff55b706fe04aa3e16
Golden Ratio Staking <95374052+Golden-Ratio-Staking@users.noreply.github.com>
2022-10-27 10:38:31 -0700

    Update and rename editvalidator.json to editvalidator.sh

--- scripts/editvalidator.sh
+++ scripts/editvalidator.sh
@@ -1,9 +1,12 @@
+#!bin/bash
+
 "$DAEMON_NAME" tx staking edit-validator \
   --chain-id="$CHAIN_ID" \
   --commission-rate="0.05" \
   --details="Bare Metal Interchain Relayer and Validator. Powered by the most beautiful number in the Cosmos!" \
   --from="$WALLET" \
   --gas="$GAS" \
+  --gas-adjustment= "1.3" \
   --gas-prices="$GAS_PRICES""$DENOM" \
   --security-contact="info@goldenstaking.com" \
   --website="https://www.goldenstaking.com"
