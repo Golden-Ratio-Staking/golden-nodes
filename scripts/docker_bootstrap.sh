5a4d0b63eab6ca498c6fe7df1b9667a88e0ea94b
Golden Ratio Staking <95374052+Golden-Ratio-Staking@users.noreply.github.com>
2022-11-04 11:09:35 -0700

    Update docker_bootstrap.sh

--- scripts/docker_bootstrap.sh
+++ scripts/docker_bootstrap.sh
@@ -14,13 +14,17 @@ sudo apt-get install \
     lsb-release
 
 # Get Docker Official GPG Key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
 # Get Docker Stable
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # Install Docker Engine
  sudo apt-get update
  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+ 
+# Test hello-world
+sudo docker run hello-world
