6c803c8fc5c9a1a45dd5cb5a77c6232658b1a8f8
Golden Ratio Staking <95374052+Golden-Ratio-Staking@users.noreply.github.com>
2022-11-06 09:12:37 -0800

    Create ip_list.sh

---
+++ infrastructure/templates/nginx/ip_list.sh
@@ -0,0 +1,27 @@
+#!bin/bash
+
+# IPv4
+sudo ufw allow from 173.245.48.0/20
+sudo ufw allow from 103.21.244.0/22
+sudo ufw allow from 103.22.200.0/22
+sudo ufw allow from 103.31.4.0/22
+sudo ufw allow from 141.101.64.0/18
+sudo ufw allow from 108.162.192.0/18
+sudo ufw allow from 190.93.240.0/20
+sudo ufw allow from 188.114.96.0/20
+sudo ufw allow from 197.234.240.0/22
+sudo ufw allow from 198.41.128.0/17
+sudo ufw allow from 162.158.0.0/15
+sudo ufw allow from 104.16.0.0/13
+sudo ufw allow from 104.24.0.0/14
+sudo ufw allow from 172.64.0.0/13
+sudo ufw allow from 131.0.72.0/22
+
+# IPv6
+sudo ufw allow from 2400:cb00::/32
+sudo ufw allow from 2606:4700::/32
+sudo ufw allow from 2803:f800::/32
+sudo ufw allow from 2405:b500::/32
+sudo ufw allow from 2405:8100::/32
+sudo ufw allow from 2a06:98c0::/29
+sudo ufw allow from 2c0f:f248::/32
