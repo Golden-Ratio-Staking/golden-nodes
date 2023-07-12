abb28e225d2f0f307e7e346c161dedab2a756bfc
Golden Ratio Staking <95374052+Golden-Ratio-Staking@users.noreply.github.com>
2023-07-12 09:21:27 -0400

    Update docker_bootstrap.sh

--- scripts/docker_bootstrap.sh
+++ scripts/docker_bootstrap.sh
@@ -27,7 +27,7 @@ echo \
  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
  
 # Get Docker-Compose
-sudo curl -SL https://github.com/docker/compose/releases/download/v2.12.2/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+sudo curl -SL https://github.com/docker/compose/releases/download/v2.20.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
  
 # Test hello-world
 sudo docker run hello-world
